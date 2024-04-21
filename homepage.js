d1cbf8e67c28f9f13e2e4131b9d1805cdff0fd0b
minhquan18 <lekimminhquan@gmail.com>
2024-04-21 22:01:37 +0700

    update

--- homepage.js
+++ homepage.js
@@ -1,2 +1,13 @@
-const nodeList = document.querySelectorAll("minhquan");
-console.log(nodeList);
+const nodeList = document.querySelectorAll('.menu__item');
+const windonwlink = window.location.pathname;
+
+nodeList.forEach(nodeLists => {
+    if (nodeLists.href.includes(windonwlink))
+    {
+        nodeLists.classList.add("active");
+    }
+});
+
+
+
+

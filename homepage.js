b53c3441529363130c8a3c0fadc9aa0d9fa76fd2
minhquan18 <lekimminhquan@gmail.com>
2024-05-02 00:59:12 +0700

    update

--- homepage.js
+++ homepage.js
@@ -1,4 +1,5 @@
-const intro = setTimeout(Intro,3950);
+
+const intro = setTimeout(Intro,3850);
 
 function Intro(){
    const y = document.getElementById("intro");
@@ -18,8 +19,51 @@ nodeList.forEach(nodeLists => {
 });
 
 
-const pos = document.getElementById('menu').style.location;
-console.log(pos);
+
+
+setTimeout(Type,6000);
+
+function Type() {
+    const z = document.getElementById('object');
+    z.style.visibility = "visible";
+    z.classList.remove("Removetyping");
+    z.classList.add("type");
+    setTimeout(removetyping,3000);
+}
+function Removetype(){
+    let y = document.getElementById('object');
+    y.classList.remove("type");
+    y.classList.add("Removetyping");
+    setTimeout(Type,3000);
+}
+
+function removetyping(){
+    let y = document.getElementById('object');
+    y.classList.remove("type");
+    setTimeout(Removetype,5000);
+}
+
+
+setTimeout(Image1,6000);
+function Image1(){
+    const images = document.getElementById('imagee');
+    images.classList.remove('image2');
+    images.classList.add("image1");
+    setTimeout(holdimage,3000);
+}
+
+function Image2(){
+    const imagess = document.getElementById('imagee');
+    imagess.classList.remove("image3");
+    imagess.classList.add("image2");
+    setTimeout(Image1,3000);
+}
+function holdimage(){
+    const p = document.getElementById('imagee');
+    p.classList.remove('image1');
+    p.classList.add('image3');
+    setTimeout(Image2,5000);
+}
 
 
 
